a5eb52b192db0643b93b8df545413d1c8b5c2689
Rohit Nikam <nikam.m.rohit@gmail.com>
2026-03-21 17:49:35 -0500

    Initial Commit

---
+++ Rover/RoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoverApp.swift
+//  Rover
+//
+//  Created by Rohit Nikam on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct RoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
